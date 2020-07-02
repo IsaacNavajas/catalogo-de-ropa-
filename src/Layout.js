c9b60e70b5a63c9c25204fca64de81d569731ff0
isaac <isaacnavajas@gmail.com>
2020-07-02 22:19:24 +0200

    problema navbar

--- src/Layout.js
+++ src/Layout.js
@@ -11,12 +11,12 @@ class Layout extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <div > 
+                <div className="container-fluid" > 
 
                     <Navbar/>
-                        <section  className="container-fluid">
-                            {this.props.children}
-                        </section>
+                    
+                    {this.props.children}
+                  
                 </div>
 
                 
